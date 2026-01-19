516148bb6adcbdc4d020cf5280fc440720ad6218
Brian-Tiedjen <Briantiedjen@gmail.com>
2026-01-19 13:59:31 -0500

    Import step to repo

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -3,9 +3,6 @@ resource "aws_ecr_repository" "app" {
   name = var.repository_name
   image_tag_mutability = "IMMUTABLE"
 
-    lifecycle {
-    prevent_destroy = true
-    }
   image_scanning_configuration {
     scan_on_push = true
   }
