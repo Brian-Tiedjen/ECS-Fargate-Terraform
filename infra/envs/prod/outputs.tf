66e0545cf7278ec1ef93393826f92d4027c22494
Brian-Tiedjen <Briantiedjen@gmail.com>
2026-01-19 19:52:42 -0500

    Cleaning up output

--- infra/envs/prod/outputs.tf
+++ infra/envs/prod/outputs.tf
@@ -1,11 +1,3 @@
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
-output "public_subnet_ids" {
-  value = module.vpc.public_subnet_ids
-}
-
 output "alb_url" {
   value = module.alb.alb_url
 }
@@ -16,24 +8,4 @@ output "alb_dns_name" {
 
 output "ecr_repository_url" {
   value = module.ecr.repository_url
-}
-
-output "ecs_cluster_name" {
-  value = module.ecs.cluster_name
-}
-
-output "ecs_service_name" {
-  value = module.ecs.service_name
-}
-
-output "logs_bucket_name" {
-  value = module.logs.logs_bucket_name
-}
-
-output "ecs_task_execution_role_arn" {
-  value = module.iam.task_execution_role_arn
-}
-
-output "ecs_task_role_arn" {
-  value = module.iam.task_role_arn
 }
