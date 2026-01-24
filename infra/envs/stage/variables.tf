d087f11503fa821bcf6d61710fe84de173a11880
Brian-Tiedjen <Briantiedjen@gmail.com>
2026-01-24 15:28:09 -0500

    Add monitoring module and related variables for ECS service to staging

--- infra/envs/stage/variables.tf
+++ infra/envs/stage/variables.tf
@@ -81,6 +81,46 @@ variable "scale_out_cooldown" {
   default = 60
 }
 
+variable "alarm_topic_name" {
+  type    = string
+  default = ""
+}
+
+variable "alarm_email_subscriptions" {
+  type    = list(string)
+  default = []
+}
+
+variable "cpu_high_threshold" {
+  type    = number
+  default = 75
+}
+
+variable "memory_high_threshold" {
+  type    = number
+  default = 80
+}
+
+variable "evaluation_periods" {
+  type    = number
+  default = 2
+}
+
+variable "period_seconds" {
+  type    = number
+  default = 60
+}
+
+variable "enable_dashboard" {
+  type    = bool
+  default = true
+}
+
+variable "dashboard_name" {
+  type    = string
+  default = ""
+}
+
 variable "task_cpu" {
   type    = number
   default = 256
